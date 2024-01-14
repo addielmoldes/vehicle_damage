6cd70eb06d73bc5d5686d499a614418ceb745c4d
addiel <addielpunk99@gmail.com>
2024-01-14 04:30:59 -0500

    implementando canvas y exportar - cargar json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,12 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def edit_damage
+
+  end
+
+  def save_damage
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+  get 'edit_damage', to: 'home#edit_damage'
+  post 'save_damage', to: 'home#save_damage'
 end
